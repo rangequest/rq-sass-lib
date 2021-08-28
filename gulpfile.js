5897cbe828b0412ef282a63daf87603eb08f2104
Range Quest <range.quest@gmail.com>
2021-08-28 22:00:20 +0530

    organized sass framework files to a new rqcss directory. introduced the partials to the system.

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src('index.scss')
+  return src('rqcss/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['index.scss'], buildStyles)
+  watch(['rqcss/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
